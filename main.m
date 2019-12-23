540d623f821c4a23a770fd2643402b0ffd06da6e
Asaf90 <58466785+Asaf90@users.noreply.github.com>
2019-12-23 11:01:21 +0200

    Create main.m

---
+++ main.m
@@ -0,0 +1,30 @@
+%%Wireless Communication Networks Hackathon 2
+close all
+clear variables
+clc
+
+%% Phase 1: Ray Matricec - ABCD Matrix
+
+
+%% Phase 2: Spatial Propagation
+
+
+%% Phase 3: Beam Expander and Concentrator
+
+
+%% Phase 4: The Communication Link
+
+
+%% Phase 5: The Communication Link
+
+
+%% Phase 6: Filter
+
+
+%% Phase 7: Link Budget
+
+
+%% Phase 8: Detector
+
+
+%% Phase 9: Ideal Receiver
